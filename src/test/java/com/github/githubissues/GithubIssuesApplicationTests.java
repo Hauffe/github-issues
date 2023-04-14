759f564e3726081108b58be883dafaa656f3fd92
Alexandre <alex_hauffe@hotmail.com>
2023-04-14 12:32:00 -0300

    first commit

---
+++ src/test/java/com/github/githubissues/GithubIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.githubissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIssuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
